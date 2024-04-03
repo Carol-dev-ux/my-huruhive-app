feb8e5a7d171399c8ee11a43beee9e302849fcf5
Caroline Wangari <karehwachira@gmail.com>
2024-04-03 10:05:31 +0300

    About page

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,13 @@
+// components/AboutPage.js
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>About HURUHIVE TECHTOTS</h2>
+      <p>HURUHIVE TECHTOTS offers coding classes specially designed for kids to learn programming in a fun and interactive way.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 HURUHIVE TECHTOTS. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
